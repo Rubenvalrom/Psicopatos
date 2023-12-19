e9dccf20ae7d56b061c2b51072ac0e486a13be54
= <=>
2023-12-19 10:27:37 +0100

    Función generadora de posiciones (1 al 4)

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+var repeticiones = 1;
+
+function repetir(repeticiones) {
+    var posiciones = Array();
+    for (var i = 0; i < repeticiones; i++) {
+        posiciones.push(random(1, 4));
+    }
+   return posiciones;
+}
+
